b294e27bd43bb0b23e2a4e147462a04cf5122608
nnasaa88 <nasaa88@gmail.com>
2022-04-26 01:55:01 +0800

    0426-0200 flat -d search duhuv

--- database/Mymodal.js
+++ database/Mymodal.js
@@ -1,75 +1,77 @@
-// import React, { useState } from "react";
-// import {
-//   StyleSheet,
-//   Text,
-//   View,
-//   Modal,
-//   TouchableOpacity,
-//   SafeAreaView,
-// } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  Modal,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { Icon } from "react-native-elements";
 
-// const Mymodal1 = (props) => {
-//   //   const [rnmodalvisible, setrnmodalvisible] = useState(true);
-//   //   console.log(props.seemodal + " sdfsdfds ");
-//   //   setrnmodalvisible(props.seemodal);
-//   return (
-//     <View style={css.centeredview}>
-//       <Modal visible={props.seemodal} animationType="slide" transparent={true}>
-//         <View style={[css.centeredview, { marginTop: -200 }]}>
-//           <View style={css.modalview}>
-//             <Text
-//               style={{
-//                 fontSize: 16,
-//                 fontWeight: "bold",
-//                 alignItems: "center",
-//               }}
-//             >
-//               {" "}
-//               Modoal title{" "}
-//             </Text>
-//             <Text style={{ marginTop: 10 }}>
-//               {" "}
-//               xzcxzcXZCxzcXZCxz d fdsModoal body{" "}
-//             </Text>
-//             <View flexDirection="row" justifyContent="center">
-//               <TouchableOpacity
-//                 style={{}}
-//                 onPress={() => {
-//                   console.log("garlaa");
-//                   props.hidemodal();
-//                 }}
-//               >
-//                 <Text> Яах санаатай </Text>
-//               </TouchableOpacity>
-//               <TouchableOpacity style={{}} onPress={props.hidemodal}>
-//                 <Text> Санаатай </Text>
-//               </TouchableOpacity>
-//             </View>
-//           </View>
-//         </View>
-//       </Modal>
-//     </View>
-//   );
-// };
-// export default Mymodal1;
+const Mymodal1 = (props) => {
+  return (
+    <View style={css.centeredview}>
+      <Modal visible={props.seemodal} animationType="slide" transparent={true}>
+        <View style={[css.centeredview, { marginTop: 50 }]}>
+          <View style={css.modalview}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                {" "}
+                Томсгов{" "}
+              </Text>
+              <TouchableOpacity
+                style={{ marginBottom: 5, alignItems: "center" }}
+                onPress={() => {
+                  console.log("garlaa");
+                  props.hidemodal();
+                }}
+              >
+                <Icon name="close" />
+              </TouchableOpacity>
+            </View>
+            <Image
+              style={{
+                width: "100%",
+                height: "60%",
+                borderRadius: 100,
+              }}
+              source={{
+                uri: "https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png",
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+export default Mymodal1;
 
-// const css = StyleSheet.create({
-//   centeredview: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   modalview: {
-//     width: "90%",
-//     backgroundColor: "white",
-//     borderRadius: 10,
-//     paddingHorizontal: 20,
-//     paddingTop: 10,
-//     paddingBottom: 20,
-//     shadowColor: "#000",
-//     shadowOffset: { width: 0, height: 2 },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 3.84,
-//     elevation: 5,
-//   },
-// });
+const css = StyleSheet.create({
+  centeredview: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalview: {
+    width: "90%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    paddingBottom: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
--- scr/screens/Flatlistscreen.js
+++ scr/screens/Flatlistscreen.js
@@ -2,12 +2,16 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
   FlatList,
   TouchableHighlight,
   Alert,
+  TouchableOpacity,
 } from "react-native";
 import React, { useState, useContext, useEffect } from "react";
+import { Feather as Icon } from "@expo/vector-icons";
 import Mycontext, { fdate } from "../../context/Mycontext";
+import Mymodal1 from "../../database/Mymodal";
 import { resultdb } from "../../database/db";
 
 import { Mymodal } from "../../database/Search";
@@ -15,6 +19,7 @@ import { Mymodal } from "../../database/Search";
 const Flatlistscreen = () => {
   const mystatus = useContext(Mycontext);
   const [rnmodalvisible, setrnmodalvisible] = useState(false);
+  const [rnmodalvisible1, setrnmodalvisible1] = useState(false);
   const [items, setItems] = useState([]);
 
   useEffect(async () => {
@@ -27,12 +32,19 @@ const Flatlistscreen = () => {
   const handleClick = (name) => {
     setrnmodalvisible(true);
   };
+  const handleClick1 = (name) => {
+    setrnmodalvisible1(true);
+  };
 
   return (
     <View>
       <Mymodal
         seemodal={rnmodalvisible}
         hidemodal={() => setrnmodalvisible(false)}
+      />
+      <Mymodal1
+        seemodal={rnmodalvisible1}
+        hidemodal={() => setrnmodalvisible1(false)}
       />
       <FlatList
         ItemSeparatorComponent={
@@ -42,21 +54,58 @@ const Flatlistscreen = () => {
           ))
         }
         data={items}
+        key={items.id}
+        // renderItem={UserCard}
         renderItem={({ item, index, separators }) => (
-          <TouchableHighlight
-            key={item.index}
-            onPress={() => handleClick(item.name)}
-            onShowUnderlay={separators.highlight}
-            onHideUnderlay={separators.unhighlight}
+          <View
+            style={{
+              margin: 4,
+              backgroundColor: "#E1E43B",
+              marginRight: 40,
+              paddingHorizontal: 10,
+              paddingVertical: 4,
+              borderRadius: 10,
+              flexDirection: "row",
+              alignItems: "center",
+              height: 60,
+            }}
           >
-            <View style={{ backgroundColor: "white" }}>
-              <Text>
-                {index + 1}) {item.name} {item.color}
-                {item.tamge} {item.im} {item.desc}
-                {item.image}
-              </Text>
+            <View>
+              <TouchableOpacity onPress={() => handleClick1(item.name)}>
+                <Image
+                  style={{ width: 50, height: 50, borderRadius: 100 }}
+                  source={{
+                    uri: "https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png",
+                  }}
+                />
+              </TouchableOpacity>
             </View>
-          </TouchableHighlight>
+            <View style={{ flex: 1, paddingHorizontal: 10 }}>
+              <Text
+                style={{ fontSize: 16 }}
+              >{`${item.name} ${item.color}`}</Text>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <TouchableOpacity
+                onPress={() => {
+                  Alert.alert(`Тайлбар: ${item.desc}`);
+                }}
+              >
+                <Icon name="phone" style={{ marginLeft: 12 }} size={20} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  Alert.alert(`Төрсөн огноо ${item.bdate}`);
+                }}
+              >
+                <Icon
+                  name="message-circle"
+                  style={{ marginLeft: 14 }}
+                  size={20}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
         )}
       />
     </View>
@@ -64,5 +113,53 @@ const Flatlistscreen = () => {
 };
 
 export default Flatlistscreen;
+
+function UserCard({ item }) {
+  return (
+    <View
+      style={{
+        margin: 4,
+        backgroundColor: "#E1E43B",
+        marginRight: 40,
+        paddingHorizontal: 10,
+        paddingVertical: 4,
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        height: 60,
+      }}
+    >
+      <View>
+        <TouchableOpacity onPress={() => handleClick(item.name)}>
+          <Image
+            style={{ width: 50, height: 50, borderRadius: 100 }}
+            source={{
+              uri: "https://pbs.twimg.com/profile_images/486929358120964097/gNLINY67_400x400.png",
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 1, paddingHorizontal: 10 }}>
+        <Text style={{ fontSize: 16 }}>{`${item.name} ${item.color}`}</Text>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert(`Calling ${item.im}`);
+          }}
+        >
+          <Icon name="phone" style={{ marginLeft: 12 }} size={20} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert(`Messaging ${item.tamga}`);
+          }}
+        >
+          <Icon name="message-circle" style={{ marginLeft: 14 }} size={20} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({});
