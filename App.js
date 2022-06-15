93a50c69bf3955ae36c85dd7cc7b724e698e8db2
amaraaamar003 <amaraaamar003@gmail.com>
2022-06-15 21:45:12 +0800

    amar test

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { StyleSheet, Alert } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-//amaras comment dwad awdwad
+//amaras comment dwad awdwaddawdwad
 // import {createNativeStackNavigator, HeaderBackButton,} from "@react-navigation/native-stack";
 import Stacknav from "./scr/screens/Stacknav";
 // import Drawernav from "./scr/screens/DrawerNav";
